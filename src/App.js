50ce0b295fdcd4990b38aaad69fbba11856d1410
giftormdh <giftoramadha@gmail.com>
2024-06-11 02:12:34 +0700

    add: map title styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import socketIOClient from 'socket.io-client';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import React, { useEffect, useState } from "react";
+import socketIOClient from "socket.io-client";
+import { MapContainer, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import DynamicMarker from './DynamicMarker';
-import PanelData from './PanelData'
+import DynamicMarker from "./DynamicMarker";
+import PanelData from "./PanelData";
 
-const ENDPOINT = 'http://localhost:4000';
+const ENDPOINT = "http://localhost:4000";
 
 function App() {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [displayDataIndex, setDisplayDataIndex] = useState(0);
-  const [toggleDisplayData, setToggleDisplayData] = useState(true)
+  const [toggleDisplayData, setToggleDisplayData] = useState(true);
 
   useEffect(() => {
     const socket = socketIOClient(ENDPOINT);
-    
-    socket.on('geoinfo', (receivedData) => {
-        setData(receivedData.features)
-        setLoading(true)
+
+    socket.on("geoinfo", (receivedData) => {
+      setData(receivedData.features);
+      setLoading(true);
     });
 
     return () => {
@@ -27,48 +27,37 @@ function App() {
   }, []);
 
   const handleClick = (index) => {
-    console.log('Marker clicked:', index);
-    setDisplayDataIndex(index)
+    console.log("Marker clicked:", index);
+    setDisplayDataIndex(index);
   };
 
-  console.log(displayDataIndex)
+  console.log(displayDataIndex);
 
   return (
     <div>
-      <h1>Data Stream App</h1>
-      {
-        loading && 
-        toggleDisplayData ? 
-        <div className='absolute z-50 h-full w-72 top-0 left-0 flex justify-center items-center'>
-          <PanelData properties={data[displayDataIndex].properties}/>
-        </div> 
-        : 
+      <div className="absolute z-50 left-1/2 -translate-x-1/2 text-white rounded-lg bg-blue-500 text-3xl font-bold p-4">Data Stream App</div>{" "}
+      {loading && toggleDisplayData ? (
+        <div className="absolute z-50 top-1/2 -translate-y-1/2 shadow-lg ml-10">
+          <PanelData properties={data[displayDataIndex].properties} />
+        </div>
+      ) : (
         <></>
-      }
+      )}
       <div class="w-full h-screen relative z-0">
         <div style={{ height: "400px", width: "100wh" }}>
-          <MapContainer
-            center={[-7.7854, 110.3353]}
-            zoom={10}
-            scrollWheelZoom={true}
-            style={{ height: '100vh', width: '100wh' }}
-          >
-            <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            {
-              loading && data.map((point, index) => (
-                <DynamicMarker 
-                  key={index} 
-                  position={[point.geometry.coordinates[1], point.geometry.coordinates[0]]} 
+          <MapContainer center={[-7.7854, 110.3353]} zoom={10} scrollWheelZoom={true} style={{ height: "100vh", width: "100wh" }}>
+            <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            {loading &&
+              data.map((point, index) => (
+                <DynamicMarker
+                  key={index}
+                  position={[point.geometry.coordinates[1], point.geometry.coordinates[0]]}
                   properties={point.properties}
                   index={index}
                   handleClick={handleClick} // Pass handleClick as prop
                   setToggleDisplayData={setToggleDisplayData}
                 />
-              ))
-            }
+              ))}
           </MapContainer>
         </div>
       </div>
