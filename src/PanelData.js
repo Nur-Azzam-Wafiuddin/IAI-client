1d2900aedb755fe3ed442e134601f2c27002c2d4
giftormdh <giftoramadha@gmail.com>
2024-06-11 02:12:52 +0700

    add : information details style

--- src/PanelData.js
+++ src/PanelData.js
@@ -1,24 +1,50 @@
-import React from 'react';
+import React from "react";
 
 const PanelData = ({ properties }) => {
-    // Destructure properties from props if needed
-    console.log(properties)
+  // Destructure properties from props if needed
+  console.log(properties);
 
-    const { location, air_quality_index, pm10, pm2_5, o3, no2, so2, co, timestamp } = properties;
+  const { location, air_quality_index, pm10, pm2_5, o3, no2, so2, co, timestamp } = properties;
 
-    return (
-        <div className='h-5/6 w-10/12 bg-white rounded-lg p-4'>
-            <h2 className='text-lg font-bold mb-2'>{location}</h2>
-            <p>Air Quality Index: {air_quality_index}</p>
-            <p>PM10: {pm10}</p>
-            <p>PM2.5: {pm2_5}</p>
-            <p>O3: {o3}</p>
-            <p>NO2: {no2}</p>
-            <p>SO2: {so2}</p>
-            <p>CO: {co}</p>
-            <p>Timestamp: {timestamp}</p>
+  return (
+    <div className="w-9/10 bg-white rounded-lg p-4">
+      <h2 className="text-xl text-center font-bold mb-2">{location}</h2>
+      {/* air quality */}
+      <div className="text-center py-2 bg-sky-500 rounded-md mb-2 text-white">
+        Air Quality Index
+        <div className="white font-bold text-5xl"> {air_quality_index}</div>
+      </div>
+      {/* 2pm */}
+      <div className="flex justify-between text-center text-white gap-2 mb-2">
+        <div className="w-full bg-purple-400 p-4 rounded-xl">
+          PM10 <div className="font-bold text-2xl">{pm10}</div>
         </div>
-    );
-}
+        <div className="w-full bg-violet-400 p-4 rounded-xl">
+          PM2.5 <div className="font-bold text-2xl">{pm2_5}</div>
+        </div>
+      </div>
+      {/* onosoco */}
+      <div className="flex justify-between font-bold text-center gap-4 mb-4">
+        <div>
+          <div className="bg-orange-400 w-min-14 p-4 rounded-full text-white">{o3}</div>
+          <div className="text-xl">O3</div>
+        </div>
+        <div>
+          <div className="bg-amber-400 w-min-14 w-full p-4 rounded-full text-white">{no2}</div>
+          <div className="text-xl">NO2</div>
+        </div>
+        <div>
+          <div className="bg-yellow-400 w-min-14 w-full p-4 rounded-full text-white">{so2}</div>
+          <div className="text-xl">SO2</div>
+        </div>
+        <div>
+          <div className="bg-lime-400 w-min-14 w-full p-4 rounded-full text-white">{co}</div>
+          <div className="text-xl">CO</div>
+        </div>
+      </div>
+      <p>Timestamp: {timestamp}</p>
+    </div>
+  );
+};
 
 export default PanelData;
