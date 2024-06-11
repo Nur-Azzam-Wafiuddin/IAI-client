8a2d1e79de5733b6796f2a632e6d52409880625f
Nur-Azzam-Wafiuddin <nur.azzam.wafiuddin@mail.ugm.ac.id>
2024-06-11 14:52:50 +0700

    feat: user geolocation

--- src/MapMenu.js
+++ src/MapMenu.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const MapMenu = ({mapRef}) => {
+const MapMenu = ({mapRef, userPosition}) => {
 
     const map = mapRef.current;
 
@@ -8,11 +8,19 @@ const MapMenu = ({mapRef}) => {
         map.flyTo([-7.819405, 110.357586], 12)
     }
 
+    const handleFlyUser = () => {
+        map.flyTo(userPosition, 16)
+    }
+
+
 
     return (
         <div className="absolute w-52 h-16 right-5 top-0 flex justify-end items-center gap-5">
             <div className="bg-white h-10 w-10 rounded-md z-50 cursor-pointer flex items-center justify-center drop-shadow-md hover:bg-slate-100" onClick={() => handleFly()}>
                 <img src="/zoom_out.svg" alt="Icon" className="w-8 h-8" />            
+            </div>
+            <div className="bg-white h-10 w-10 rounded-md z-50 cursor-pointer flex items-center justify-center drop-shadow-md hover:bg-slate-100" onClick={() => handleFlyUser()}>
+                <img src="/location.svg" alt="Icon" className="w-8 h-8" />            
             </div>
       </div>
     );
